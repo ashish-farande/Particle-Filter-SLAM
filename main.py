988471b8f85861a0dd6f1055a28dc0440b93552c
Ashish Farande <ashish.farande@gmail.com>
2022-02-26 08:12:57 -0800

    Improve File Path Handling

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
 from ParticleFilter import *
 from Sensor import *
 
-
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 DATA_PATH = os.path.join(PROJECT_PATH, 'data')
 SENSOR_DATA_PATH = os.path.join(DATA_PATH, 'sensor_data')
 PARAM_PATH = os.path.join(DATA_PATH, 'param')
 
-LIDAR_DATA_FILE = 'lidar.csv'
-FOG_DATA_FILE = 'fog.csv'
-ENCODER_DATA_FILE = 'encoder.csv'
+LIDAR_DATA_FILE = os.path.join(SENSOR_DATA_PATH, 'lidar.csv')
+FOG_DATA_FILE = os.path.join(SENSOR_DATA_PATH, 'fog.csv')
+ENCODER_DATA_FILE = os.path.join(SENSOR_DATA_PATH, 'encoder.csv')
 
-VEHICLE_TO_LIDAR = 'Vehicle2Lidar.txt'
-VEHICLE_TO_FOG = 'Vehicle2FOG.txt'
-VEHICLE_TO_STEREO = 'Vehicle2Stereo.txt'
+LIDAR_TO_VEHICLE_PARAMETERS_PATH = os.path.join(PARAM_PATH, 'Vehicle2Lidar.txt')
+FOG_TO_VEHICLE_PARAMETERS_PATH = os.path.join(PARAM_PATH, 'Vehicle2FOG.txt')
+STEREO_TO_VEHICLE_PARAMETERS_PATH = os.path.join(PARAM_PATH, 'Vehicle2Stereo.txt')
 
-LEFT_CAMERA_CONFIG = "left_camera.yaml"
-RIGHT_CAMERA_CONFIG = "right_camera.yaml"
+LEFT_CAMERA_CONFIG_FILE_PATH = os.path.join(PARAM_PATH, "left_camera.yaml")
+RIGHT_CAMERA_CONFIG_FILE_PATH = os.path.join(PARAM_PATH, "right_camera.yaml")
 
 # Create Sensors
-lidar = Lidar(param_file=os.path.join(PARAM_PATH, VEHICLE_TO_LIDAR))
-drive = Drive(param_file=os.path.join(PARAM_PATH, VEHICLE_TO_FOG))
+lidar = Lidar(param_file=LIDAR_TO_VEHICLE_PARAMETERS_PATH)
+drive = Drive(param_file=FOG_TO_VEHICLE_PARAMETERS_PATH)
 
 # Set Up for the sensors
 lidar.load_data(os.path.join(SENSOR_DATA_PATH, LIDAR_DATA_FILE))
-drive.load_data(gyro_path=os.path.join(SENSOR_DATA_PATH, FOG_DATA_FILE), encoder_data=os.path.join(SENSOR_DATA_PATH, ENCODER_DATA_FILE))
+drive.load_data(gyro_path=FOG_DATA_FILE, encoder_data=ENCODER_DATA_FILE)
 
 # Create Particle Filter
 pf = ParticleFilter(lidar_sensor=lidar, motion_sensor=drive)
@@ -33,7 +32,6 @@ pf = ParticleFilter(lidar_sensor=lidar, motion_sensor=drive)
 # Initialise Map
 pf.initialise_map()
 pf.show_map()
-
 
 move_ts = pf.drive.get_next_timestamp()
 lidar_ts = pf.lidar.get_next_timestamp()
